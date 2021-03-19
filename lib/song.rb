7d848e1740b8ea45262706a22604b8872dc5b1be
Milan <milanvpage@gmail.com>
2021-03-18 22:49:58 -0700

    commit tree fe997fa5f2dfd682fcad9e4a6d415abcaa0892dd

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,66 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    #song.artist_name = artist_name
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    #if self.find_by_name(name)
+     # self.find_by_name(name)
+    #else
+     # self.create_by_name(name)
+     #we can istead use an or operator || to compare
+     ##if the left side is true then it won't run the right side unless the left side is false
+     self.find_by_name(name) || self.create_by_name(name)
+  end
+
+    def self.alphabetical
+      #@@all.sort{|a,b| a.name <=> b.name}
+      #or refactor and use "sort_by"
+      self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    split_filename = filename.chomp(".mp3").split(" - ")
+    song = self.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    split_filename = filename.chomp(".mp3").split(" - ")
+    song = self.new
+    song.name = split_filename[1]
+    song.artist_name = split_filename[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    #@@all.clear
+    #@@all = []
+    self.all.clear
+  end
 end
